a3a4fe960cafc82c07dbee107916b22a413a7e61
sridhar-s-subramanian <subramaniansridhar95@gmail.com>
2023-11-26 14:03:00 +0530

    feat: Initial commit
    Initial Commit
    
    BREAKING CHANGE:

---
+++ resources/js/Pages/Main.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Main = () => {
+    return (
+        <h1 className="text-amber-950 text-2xl font-extrabold">Welcome To Inertia React!</h1>
+    )
+}
+
+export default Main
